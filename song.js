afd982c8b5a053a5bff34b2259cc7d129800b7a6
liufan262 <liufan262@gmail.com>
2018-10-08 20:40:58 +0800

    new

---
+++ song.js
@@ -0,0 +1,30 @@
+$(function () {
+
+    $.get('./lyric.json').then(function(object){
+        let {lyric} = object
+        let array = lyric.split('\n')
+        let regex = /^\[(.+)\](.*)$/
+
+        array = array.map(function(string, index){
+            let matches = string.match(regex)
+            if(matches){
+                return {time: matches[1], words: matches[2]}
+            }
+        })
+        let $lyric = $('.lyric')
+        array.map(function(object){
+            if(!object){return}
+            let $p = $('<p/>')
+            $p.attr('data-time', object.time).text(object.words)
+            $p.appendTo($lyric.children('.lines'))
+        })
+    })
+
+    let audio = document.createElement('audio')
+    audio.src = 'http://isure.stream.qqmusic.qq.com/C4000031Owpo1DSLye.m4a?guid=5923384933&vkey=51C4424C7BE3A8C5EC20DA119D3B43B1DECEC52AFD4CFB86C890A7F5393217E3B7EA84F371332A885ED900E4DC36FC2A8A28E32C3180FBA5&uin=0&fromtag=66'
+    audio.oncanplay = function(){
+        audio.play()
+        $('.disc-container').addClass('playing')
+    }
+
+});
